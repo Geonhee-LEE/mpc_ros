94282625bcc36d536f90c3f53674f9f42d775b91
Geonhee-LEE <gunhee6392@gmail.com>
2020-07-02 15:53:23 +0900

    Separated the reference trajectories faccording to launch argument as 'trajectory_type'

--- mpc_ros/script/mpc_trajectory_generation.py
+++ mpc_ros/script/mpc_trajectory_generation.py
@@ -21,6 +21,7 @@ sum_error = 0
 error_x = 0
 
 id = "odom"
+trajectory_type = ""
 
 current_path_x, current_path_y, current_path_theta = 0, 0, 0
 
@@ -72,188 +73,146 @@ def generation_error_path():
 
 
 def generation_desired_path():
+    '''
+    Create desired path
+    '''
+    global trajectory_type
     rospy.loginfo("generation_desired_path()")
     
-    #global desired_path
-
     desired_path = Path()
     iter = 1000
     
-    #lemniscate
-    '''
-    '''
-    '''
-    
-    period = 1000
-    for t in range(0, iter):
-        desired_path.header.stamp = rospy.get_rostime()
-        desired_path.header.frame_id = id
-        desired_path.header.seq = t
+    if trajectory_type == "circle":
+        #circle        
+        radius = 5
+        period = 1000
+        for t in range(0, iter):
+            desired_path.header.stamp = rospy.get_rostime()
+            desired_path.header.frame_id = id
+            desired_path.header.seq = t
 
-        pose = PoseStamped()
-        #pose.header.seq = t 
-        pose.header.frame_id = id
-        #pose.header.stamp = rospy.get_rostime()
-        pose.pose.position.x = 5 * cos(2 * pi* t / period) / (sin(2 * pi * t / period) ** 2 + 1)
-        pose.pose.position.y = 5 * sin(2 * pi* t / period) * cos(2 * pi* t / period) / (sin(2 * pi * t / period) ** 2 + 1)
+            pose = PoseStamped()
+            pose.header.seq = t 
+            pose.header.frame_id = id
+            pose.header.stamp = rospy.get_rostime()
+            pose.pose.position.x = radius * sin(2 * pi * t / period) #+ self.x_0
+            pose.pose.position.y = -radius * cos(2 * pi * t / period) #+ self.y_0+
+            grad =  atan2((-radius * cos(2 * pi * (t+1) / period)- pose.pose.position.y) , (radius * sin(2 * pi * (t + 1) / period)) - (pose.pose.position.x + 1e-5))
+            q = quaternion_from_euler(0, 0, grad)
+            pose.pose.orientation.x = q[0]
+            pose.pose.orientation.y = q[1]
+            pose.pose.orientation.z = q[2]
+            pose.pose.orientation.w = q[3]
+
+            desired_path.poses.append(pose)
         
-        grad =  atan2((5 * sin(2 * pi* (t+1) / period) * cos(2 * pi* (t+1) / period) / (sin(2 * pi * (t+1) / period) ** 2 + 1)- pose.pose.position.y) , (5 * cos(2 * pi* (t+1) / period) / (sin(2 * pi * (t+1) / period) ** 2 + 1)-pose.pose.position.x + 1e-5))
-        q = quaternion_from_euler(0, 0, grad)
-        pose.pose.orientation.x = q[0]
-        pose.pose.orientation.y = q[1]
-        pose.pose.orientation.z = q[2]
-        pose.pose.orientation.w = q[3]
-
-        desired_path.poses.append(pose)
-    '''
-    '''
-    '''
-    
-
-    #circle
-    '''
-    
-    radius = 5
-    period = 1000
-    for t in range(0, iter):
-        desired_path.header.stamp = rospy.get_rostime()
-        desired_path.header.frame_id = id
-        desired_path.header.seq = t
-
-        pose = PoseStamped()
-        pose.header.seq = t 
-        pose.header.frame_id = id
-        pose.header.stamp = rospy.get_rostime()
-        pose.pose.position.x = radius * sin(2 * pi * t / period) #+ self.x_0
-        pose.pose.position.y = -radius * cos(2 * pi * t / period) #+ self.y_0+
-        grad =  atan2((-radius * cos(2 * pi * (t+1) / period)- pose.pose.position.y) , (radius * sin(2 * pi * (t + 1) / period)) - (pose.pose.position.x + 1e-5))
-        q = quaternion_from_euler(0, 0, grad)
-        pose.pose.orientation.x = q[0]
-        pose.pose.orientation.y = q[1]
-        pose.pose.orientation.z = q[2]
-        pose.pose.orientation.w = q[3]
-
-        desired_path.poses.append(pose)
-    
-    '''
-
     #epitrochoid
-    
-    '''
-    
-    R = 5
-    r = 1
-    d = 3
-    period = 1000
-    scale_factor = 1  
-    for t in range(0, iter):
-        desired_path.header.stamp = rospy.get_rostime()
-        desired_path.header.frame_id = id
-        desired_path.header.seq = t
+    if trajectory_type == "epitrochoid":
+        R = 5
+        r = 1
+        d = 3
+        period = 1000
+        scale_factor = 1  
+        for t in range(0, iter):
+            desired_path.header.stamp = rospy.get_rostime()
+            desired_path.header.frame_id = id
+            desired_path.header.seq = t
 
-        pose = PoseStamped()
-        pose.header.seq = t
-        pose.header.frame_id = id
-        pose.header.stamp = rospy.get_rostime()
-        pose.pose.position.x = scale_factor * ((R + r) * cos(2 * pi * t/ period) - d * cos(((R + r) / r) * 2 * pi * t / period))
-        pose.pose.position.y = scale_factor * ((R + r) * sin(2 * pi * t/ period) - d * sin(((R + r) / r) * 2 * pi * t / period))
-        grad =  atan2((5 * sin(2 * pi* (t+1) / period) * cos(2 * pi* (t+1) / period) / (sin(2 * pi * (t+1) / period) ** 2 + 1)- pose.pose.position.y) , (5 * cos(2 * pi* (t+1) / period) / (sin(2 * pi * (t+1) / period) ** 2 + 1)-pose.pose.position.x + 1e-5))
-        q = quaternion_from_euler(0, 0, grad)
-        pose.pose.orientation.x = q[0]
-        pose.pose.orientation.y = q[1]
-        pose.pose.orientation.z = q[2]
-        pose.pose.orientation.w = q[3]
-        
-        
-        desired_path.poses.append(pose)
+            pose = PoseStamped()
+            pose.header.seq = t
+            pose.header.frame_id = id
+            pose.header.stamp = rospy.get_rostime()
+            pose.pose.position.x = scale_factor * ((R + r) * cos(2 * pi * t/ period) - d * cos(((R + r) / r) * 2 * pi * t / period))
+            pose.pose.position.y = scale_factor * ((R + r) * sin(2 * pi * t/ period) - d * sin(((R + r) / r) * 2 * pi * t / period))
+            grad =  atan2((5 * sin(2 * pi* (t+1) / period) * cos(2 * pi* (t+1) / period) / (sin(2 * pi * (t+1) / period) ** 2 + 1)- pose.pose.position.y) , (5 * cos(2 * pi* (t+1) / period) / (sin(2 * pi * (t+1) / period) ** 2 + 1)-pose.pose.position.x + 1e-5))
+            q = quaternion_from_euler(0, 0, grad)
+            pose.pose.orientation.x = q[0]
+            pose.pose.orientation.y = q[1]
+            pose.pose.orientation.z = q[2]
+            pose.pose.orientation.w = q[3]        
+            
+            desired_path.poses.append(pose)
     
-    '''
+    #infinite
+    if trajectory_type == "infinite":    
+        period = 1000
+        scale_factor = 1  
+        for t in range(0, iter):
+            desired_path.header.stamp = rospy.get_rostime()
+            desired_path.header.frame_id = id
+            desired_path.header.seq = t
 
-    #ifinite
+            pose = PoseStamped()
+            pose.header.seq = t
+            pose.header.frame_id = id
+            pose.header.stamp = rospy.get_rostime()
+            pose.pose.position.x = 10 * cos(2 * pi* t / period) / (sin(2 * pi * t / period) ** 2 + 1)
+            pose.pose.position.y = 10 * sin(2 * pi* t / period) * cos(2 * pi* t / period) / (sin(2 * pi * t / period) ** 2 + 1)
+            grad =  atan2((10 * cos(2 * pi* (t+1) / period) / (sin(2 * pi * (t+1) / period) ** 2 + 1)- pose.pose.position.y)\
+                        ,10 * sin(2 * pi* (t+1) / period) * cos(2 * pi* (t+1) / period) / (sin(2 * pi * (t+1) / period) ** 2 + 1)-pose.pose.position.x + 1e-5)
+            q = quaternion_from_euler(0, 0, grad)
+            pose.pose.orientation.x = q[0]
+            pose.pose.orientation.y = q[1]
+            pose.pose.orientation.z = q[2]
+            pose.pose.orientation.w = q[3]      
+            
+            desired_path.poses.append(pose)
     
-    '''
-    '''
-    period = 1000
-    scale_factor = 1  
-    for t in range(0, iter):
-        desired_path.header.stamp = rospy.get_rostime()
-        desired_path.header.frame_id = id
-        desired_path.header.seq = t
-
-        pose = PoseStamped()
-        pose.header.seq = t
-        pose.header.frame_id = id
-        pose.header.stamp = rospy.get_rostime()
-        pose.pose.position.x = 10 * cos(2 * pi* t / period) / (sin(2 * pi * t / period) ** 2 + 1)
-        pose.pose.position.y = 10 * sin(2 * pi* t / period) * cos(2 * pi* t / period) / (sin(2 * pi * t / period) ** 2 + 1)
-        grad =  atan2((10 * cos(2 * pi* (t+1) / period) / (sin(2 * pi * (t+1) / period) ** 2 + 1)- pose.pose.position.y)\
-                      ,10 * sin(2 * pi* (t+1) / period) * cos(2 * pi* (t+1) / period) / (sin(2 * pi * (t+1) / period) ** 2 + 1)-pose.pose.position.x + 1e-5)
-        q = quaternion_from_euler(0, 0, grad)
-        pose.pose.orientation.x = q[0]
-        pose.pose.orientation.y = q[1]
-        pose.pose.orientation.z = q[2]
-        pose.pose.orientation.w = q[3]      
-        
-        desired_path.poses.append(pose)
-    '''
-    '''
-
     #square
-    '''
-    period = 1000
-    l = 10
-    PI = 3.141592
-    x = 0.0
-    y = 0.0
-    for t in range(0, iter):
-        desired_path.header.stamp = rospy.get_rostime()
-        desired_path.header.frame_id = id
-        desired_path.header.seq = t
+    if trajectory_type == "square":
+        period = 1000
+        l = 10
+        PI = 3.141592
+        x = 0.0
+        y = 0.0
+        for t in range(0, iter):
+            desired_path.header.stamp = rospy.get_rostime()
+            desired_path.header.frame_id = id
+            desired_path.header.seq = t
 
-        pose = PoseStamped()
-        pose.header.seq = t
-        pose.header.frame_id = id
-        pose.header.stamp = rospy.get_rostime()
+            pose = PoseStamped()
+            pose.header.seq = t
+            pose.header.frame_id = id
+            pose.header.stamp = rospy.get_rostime()
 
-        if(t <= period * 0.25):
-            x = 0
-            y += l/(period*0.25)
-            pose.pose.position.x = x
-            pose.pose.position.y = y
-            q = quaternion_from_euler(0, 0, PI/2)
-            pose.pose.orientation.x = q[0]
-            pose.pose.orientation.y = q[1]
-            pose.pose.orientation.z = q[2]
-            pose.pose.orientation.w = q[3]
-        elif(t <= period * 0.5):
-            x -= l/(period*0.25)
-            pose.pose.position.x = x
-            pose.pose.position.y = y
-            q = quaternion_from_euler(0, 0, PI)
-            pose.pose.orientation.x = q[0]
-            pose.pose.orientation.y = q[1]
-            pose.pose.orientation.z = q[2]
-            pose.pose.orientation.w = q[3]
-        elif(t <= period * 0.75):
-            y -=  l/(period*0.25)
-            pose.pose.position.x = x
-            pose.pose.position.y = y
-            q = quaternion_from_euler(0, 0, -PI/2)
-            pose.pose.orientation.x = q[0]
-            pose.pose.orientation.y = q[1]
-            pose.pose.orientation.z = q[2]
-            pose.pose.orientation.w = q[3]
-        elif(t <= period):
-            x += l/(period*0.25)
-            pose.pose.position.x = x
-            pose.pose.position.y = y
-            q = quaternion_from_euler(0, 0, 0)
-            pose.pose.orientation.x = q[0]
-            pose.pose.orientation.y = q[1]
-            pose.pose.orientation.z = q[2]
-            pose.pose.orientation.w = q[3]
-        desired_path.poses.append(pose)
-    '''
+            if(t <= period * 0.25):
+                x = 0
+                y += l/(period*0.25)
+                pose.pose.position.x = x
+                pose.pose.position.y = y
+                q = quaternion_from_euler(0, 0, PI/2)
+                pose.pose.orientation.x = q[0]
+                pose.pose.orientation.y = q[1]
+                pose.pose.orientation.z = q[2]
+                pose.pose.orientation.w = q[3]
+            elif(t <= period * 0.5):
+                x -= l/(period*0.25)
+                pose.pose.position.x = x
+                pose.pose.position.y = y
+                q = quaternion_from_euler(0, 0, PI)
+                pose.pose.orientation.x = q[0]
+                pose.pose.orientation.y = q[1]
+                pose.pose.orientation.z = q[2]
+                pose.pose.orientation.w = q[3]
+            elif(t <= period * 0.75):
+                y -=  l/(period*0.25)
+                pose.pose.position.x = x
+                pose.pose.position.y = y
+                q = quaternion_from_euler(0, 0, -PI/2)
+                pose.pose.orientation.x = q[0]
+                pose.pose.orientation.y = q[1]
+                pose.pose.orientation.z = q[2]
+                pose.pose.orientation.w = q[3]
+            elif(t <= period):
+                x += l/(period*0.25)
+                pose.pose.position.x = x
+                pose.pose.position.y = y
+                q = quaternion_from_euler(0, 0, 0)
+                pose.pose.orientation.x = q[0]
+                pose.pose.orientation.y = q[1]
+                pose.pose.orientation.z = q[2]
+                pose.pose.orientation.w = q[3]
+            desired_path.poses.append(pose)
 
     desired_path_pub.publish(desired_path) 
     desired_path = []
@@ -295,15 +254,16 @@ def find_line_position(path_number, y):
         current_path_x = (y - start_pt_y)*(next_pt_x - start_pt_x)/(next_pt_y - start_pt_y) + start_pt_x
         current_path_theta = atan2(next_pt_y - start_pt_y, next_pt_x - start_pt_x)
 
-rospy.init_node('path_node')
-
-odom_sub = rospy.Subscriber('/odom', Odometry, odom_cb)
-
-desired_path_pub = rospy.Publisher('/desired_path', Path, queue_size=500)
-odom_path_pub = rospy.Publisher('/recorded_path', Path, queue_size=10)
-error_path_pub = rospy.Publisher('/error_path', Path, queue_size=10)
-
 
 
 if __name__ == '__main__':
+    rospy.init_node('path_node')
+    rospy.loginfo("path_node is started!!")
+    global trajectory_type
+    trajectory_type =  rospy.get_param('~trajectory_type')
+    odom_sub = rospy.Subscriber('/odom', Odometry, odom_cb)
+
+    desired_path_pub = rospy.Publisher('/desired_path', Path, queue_size=500)
+    odom_path_pub = rospy.Publisher('/recorded_path', Path, queue_size=10)
+    error_path_pub = rospy.Publisher('/error_path', Path, queue_size=10)
     rospy.spin()
